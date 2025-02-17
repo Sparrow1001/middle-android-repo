80896c2d31a1d7d7e07e0962703c42886238d771
Evgeniy Fedorenko <evgeny.fedorenko@aliexpress.ru>
2025-02-17 22:47:23 +0300

    traditional container

--- app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.androidpracticumcustomview
 
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 import com.example.androidpracticumcustomview.ui.theme.CustomContainer
@@ -24,18 +27,25 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun startXmlPracticum() {
-        val customContainer = CustomContainer(this)
+        val customContainer = CustomContainer(this).apply {
+            layoutParams = ViewGroup.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+        }
+
         setContentView(customContainer)
 
         val firstView = TextView(this).apply {
-            // TODO
-            // ...
+            text = "Top Element"
+            setBackgroundColor(Color.RED)
+            layoutParams = ViewGroup.LayoutParams(200, 200)
         }
 
         val secondView = TextView(this).apply {
-            // TODO
-            // ...
+            text = "Bottom Element"
+            setBackgroundColor(Color.GREEN)
+            layoutParams = ViewGroup.LayoutParams(200, 200)
         }
+
+        customContainer.addView(firstView)
 
         // Добавление второго элемента через некоторое время
         Handler(Looper.getMainLooper()).postDelayed({
--- app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
+++ app/src/main/java/com/example/androidpracticumcustomview/ui/theme/CustomContainer.kt
@@ -1,5 +1,7 @@
 package com.example.androidpracticumcustomview.ui.theme
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
@@ -22,19 +24,55 @@ class CustomContainer @JvmOverloads constructor(
         setWillNotDraw(false)
     }
 
-    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        // TODO
-        // ...
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+        for (i in 0 until childCount) {
+            val child = getChildAt(i)
+            val childWidth = child.measuredWidth
+            val childHeight = child.measuredHeight
+
+            val left = (width - childWidth) / 2
+
+            val top = when (i) {
+                0 -> 0
+                else -> height - childHeight
+            }
+
+            child.layout(left, top, left + childWidth, top + childHeight)
+        }
     }
 
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        // TODO
-        // ...
+    override fun addView(child: View?) {
+        if (childCount >= 2) throw IllegalStateException("Maximum 2 children allowed")
+        super.addView(child)
+        child?.let { prepareChild(it) }
     }
 
-    override fun addView(child: View) {
-        // TODO
-        // ...
+    private fun prepareChild(child: View) {
+        child.alpha = 0f
+
+        child.post {
+            val index = indexOfChild(child)
+            val containerHeight = height.toFloat()
+            val childHeight = child.height.toFloat()
+
+            val centerOffset = (containerHeight/2 - childHeight/2)
+
+            child.translationY = when (index) {
+                0 -> centerOffset
+                else -> -centerOffset
+            }
+
+            AnimatorSet().apply {
+                playTogether(
+                    ObjectAnimator.ofFloat(child, View.ALPHA, 1f).apply {
+                        duration = 2000
+                    },
+                    ObjectAnimator.ofFloat(child, View.TRANSLATION_Y, 0f).apply {
+                        duration = 5000
+                    }
+                )
+                start()
+            }
+        }
     }
 }
